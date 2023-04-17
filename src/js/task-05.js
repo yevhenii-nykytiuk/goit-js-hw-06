8fcbc9de0d3b82b1fbbd31e1e974e5b4895b401f
Yevhenii Nykytiuk <crashbandicoot123r@gmail.com>
2023-04-17 07:32:51 +0300

    added task 5

---
+++ src/js/task-05.js
@@ -0,0 +1,13 @@
+const formNameInput = document.getElementById("name-input");
+const nameOutputSpan = document.getElementById("name-output");
+
+
+formNameInput.addEventListener("input", onFormInput);
+
+
+function onFormInput (event) {
+  nameOutputSpan.textContent = event.currentTarget.value;
+  if (nameOutputSpan.textContent === "") {
+    nameOutputSpan.textContent = "Anonymous";
+  }
+}
